df650ed23d30bed850a06f172bbd1bb163ea56b9
Alethea Toh <aletheatohh@gmail.com>
2023-05-16 01:41:25 +0800

    Cleanup

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
